db10584ca0d1bf4409e50359a7d14a35c02aa797
Swapnil Khande <swapnilkhande6996@gmail.com>
2023-07-01 08:56:24 +0530

    Added 2 unit tests with jest

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders landing page with login link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/LOGIN/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders landing page buttons list', async () => {
+  render(<App />);
+  const buttonList = await screen.findAllByRole("button");
+  expect(buttonList).toHaveLength(2);
+});
+
